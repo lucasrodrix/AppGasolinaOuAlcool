d12f9d139a1b7fc780d59d94d4c9bccd55b32fd3
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-10-04 15:37:44 -0300

    Projeto Finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  precoAlcool: any;
+  precoGasolina: any;
 
   constructor() {}
+  public resultado: String = "Resultado"
+
+  calcular(){
+    if (this.precoAlcool && this.precoGasolina) {
+      var pAlcool = this.precoAlcool = parseFloat(this.precoAlcool)
+      var pGasolina = this.precoGasolina = parseFloat(this.precoGasolina)
+
+      var res = pAlcool / pGasolina
+      if(res >= 0.7){
+        this.resultado = "Melhor Usar Gasolina"
+      }else{
+        this.resultado = "Melhor usar Álcool"
+      }
+    } else {
+      this.resultado = "Preencha Corretamente os Campos"
+    }
+  }
 
 }
